0233b892b2115b25597ab2acc007a8bdf42fa3f1
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-07-28 14:47:39 -0700

    0. Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Write a flask script that
+starts a flask application.
+"""
+
+from flask import Flask
+
+"""
+Create an instance of the flask
+application
+"""
+app = Flask(__name__)
+
+"""
+Route the URL to the home directory
+"""
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    return Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port='5000')
