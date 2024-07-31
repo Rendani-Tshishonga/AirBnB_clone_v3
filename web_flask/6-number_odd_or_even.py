99ae1c0074af247286ec2c922d38da78d2aea1a8
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-07-31 10:56:15 -0700

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -15,10 +15,6 @@ def index():
     """
     return "Hello HBNB!"
 
-"""
-We will route to /hbnb
-
-"""
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -26,53 +22,42 @@ def hbnb():
     return HBNB
     """
     return "HBNB"
-"""
-We will route to /c/<text>
-"""
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def cfun(text):
+    """Display C is fun"""
     return "C " + text.replace("_", " ")
-"""
-We will create a route for
-/python/<text>
-"""
+
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_cool(text="is cool"):
+    """Display Python is Cool"""
     return "Python " + text.replace("_", " ")
-"""
-We will create a route for
-/number/<n> ensure n is a number
-"""
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """Display n as an integer"""
     return f"{n} is a number"
-"""
-We will create a route to
-/number_template/<n>, which will render a
-HTML page only if n is an integer.
-"""
+
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def  number_temp(n):
+    """Display HTML if n is a integer"""
     return render_template('5-number.html', n=n)
 
-"""
-We will create a route to
-/number_odd_or_even/<int: n> which will render
-a HTML page only if n is integer.
-"""
 
 @app.route('/number_odd_or_even/<int: n>', strict_slahes=False)
 def number_odd_even(n):
+    """Disaplay HTML if n is integer"""
     if (n % 2 == 0):
         odd_even = "even" 
     else
         odd_even = "odd"
     return render_template('6-number_odd_or_even.html', n=n, odd_even=odd_even)
 
-"""
-Web application must listen to all
-public addresses on port 5000.
-"""
+
 if __name__ =="__main__":
     app.run(host='0.0.0.0', port='5000')
