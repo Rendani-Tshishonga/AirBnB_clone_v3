0dd4c735dc5abcb6db494e2d57f09fedf099bc6a
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-08-05 15:26:05 -0700

    update classes

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import Column, String, ForeignKey
 
+"""City inherits from BaseModel and Base"""
+class City(BaseModel, Base):
+    """Representation of __tablename__ """
+    __tablename__ = 'cities'
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
