87c10ae2eb3d0a4386fea37efe809ccecaed556e
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-08-06 15:35:25 -0700

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Load Libraries"""
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.vi.views import app_views
 from os import getenv
@@ -16,6 +16,10 @@ def teardown():
     """Teardown application"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handles a 404 error"""
+    return make_response(jsonify({'error': 'not found'}), 404)
 
 if __name__ == "__main__":
     app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,25 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.engine import storage
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ A method to return status okay"""
     data = {'status': 'ok'}
+    response = jsonify(data)
+    return response
+
+@app_views.route('/stats', strict_slashes=False)
+def obj_count():
+    """Retrieves the count of all objects"""
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cites": storage.count("City"),
+        "places": storage.count("Place"),
+        "review": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+            }
     response = jsonify(data)
     return response
